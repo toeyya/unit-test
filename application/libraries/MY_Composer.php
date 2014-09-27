01a50d47e65f86d2144c7a9edbfeeef3564a39d5
araya tansinchai <araya_tan@truecorp.co.th>
2014-09-27 20:16:57 +0700

    first commit

---
+++ application/libraries/MY_Composer.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed.');
+/**
+ * Description of MY_Composer
+ *
+ * @author Araya
+ */
+class MY_Composer
+{
+    function __construct()
+    {
+        include("./vendor/autoload.php");
+    }
+}
+?>
---
+++ application/views/tests.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: USER
+ * Date: 27/9/2557
+ * Time: 17:22 ¹.
+ * To change this template use File | Settings | File Templates.
+ */
